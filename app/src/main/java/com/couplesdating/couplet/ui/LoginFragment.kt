4e980eb3a17337b099da071a79e88f403a2298ce
Luciano Luzzi <luciano@woovapp.com>
2021-02-07 19:15:44 +0100

    Setting status bar color

--- app/src/main/java/com/couplesdating/couplet/ui/LoginFragment.kt
+++ app/src/main/java/com/couplesdating/couplet/ui/LoginFragment.kt
@@ -22,9 +22,4 @@ class LoginFragment : Fragment() {
         binding = FragmentLoginBinding.inflate(inflater)
         return binding.root
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-    }
 }
