5ec4912c48b90a0d2802a9e4c202db0a238c0dcb
Luciano Luzzi <luciano@woovapp.com>
2021-02-09 22:38:27 +0100

    Changing action bar style.

--- app/src/main/java/com/couplesdating/couplet/MainActivity.kt
+++ app/src/main/java/com/couplesdating/couplet/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.navigation.ui.navigateUp
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
