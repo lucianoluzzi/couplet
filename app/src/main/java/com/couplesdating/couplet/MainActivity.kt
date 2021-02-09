ac8cd84d1bda5fd6c90336d2a387b8dea8bf7336
Luciano Luzzi <luciano@woovapp.com>
2021-02-09 21:59:33 +0100

    Adding an action bar and setting it's navigation

--- app/src/main/java/com/couplesdating/couplet/MainActivity.kt
+++ app/src/main/java/com/couplesdating/couplet/MainActivity.kt
@@ -2,10 +2,32 @@ package com.couplesdating.couplet
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.navigateUp
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpActionBarNavigation()
+    }
+
+    private fun setUpActionBarNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragment_container_view)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
