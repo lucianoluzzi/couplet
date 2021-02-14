33bd35c691204d2eaac9822ad4202b883d117d11
Luciano Luzzi <luciano@woovapp.com>
2021-02-14 10:15:06 +0100

    Handling the gender input

--- app/src/main/java/com/couplesdating/couplet/ui/register/NameAndGenderFragment.kt
+++ app/src/main/java/com/couplesdating/couplet/ui/register/NameAndGenderFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.core.view.isVisible
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import com.couplesdating.couplet.R
 import com.couplesdating.couplet.databinding.FragmentNameAndGenderBinding
@@ -16,15 +18,23 @@ class NameAndGenderFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentNameAndGenderBinding.inflate(inflater, container, false)
+        setGenderField()
+        return binding.root
+    }
+
+    private fun setGenderField() {
+        val genderOptions = listOf("Female", "Male", "Other")
         binding.gender.setAdapter(
             ArrayAdapter(
                 requireContext(),
                 R.layout.gender_item_layout,
-                listOf("Female", "Male", "Other")
+                genderOptions
             )
         )
-        return binding.root
+        binding.gender.doOnTextChanged { text, _, _, _ ->
+            binding.otherGenderInputLayout.isVisible = text.toString() == "Other"
+        }
     }
 }
