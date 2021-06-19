d4e3bd0cbcf54666e6e1e83c3854151b0a4e92e1
Luciano Luzzi <luciano@woovapp.com>
2021-06-20 00:39:35 +0200

    Setting proper game buttons with colored shadows.

--- app/src/main/java/com/couplesdating/couplet/ui/idea/IdeaListFragment.kt
+++ app/src/main/java/com/couplesdating/couplet/ui/idea/IdeaListFragment.kt
@@ -1,5 +1,6 @@
 package com.couplesdating.couplet.ui.idea
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +8,9 @@ import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.couplesdating.couplet.R
 import com.couplesdating.couplet.databinding.FragmentIdeaListBindingImpl
+import com.couplesdating.couplet.ui.utils.CircularOutlineProvider
 
 class IdeaListFragment : Fragment() {
     private val navigationArgs by navArgs<IdeaListFragmentArgs>()
@@ -30,6 +33,7 @@ class IdeaListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setButtonsElevation()
         with(binding) {
             illustration.setImageDrawable(
                 ContextCompat.getDrawable(requireContext(), category.imageBig)
@@ -40,6 +44,26 @@ class IdeaListFragment : Fragment() {
                 ideaListFragment = this@IdeaListFragment
             )
             pager.isUserInputEnabled = false
+        }
+    }
+
+    private fun setButtonsElevation() = with(binding) {
+        val outlineProvider = CircularOutlineProvider()
+        no.outlineProvider = outlineProvider
+        yes.outlineProvider = outlineProvider
+        maybe.outlineProvider = outlineProvider
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            no.outlineAmbientShadowColor =
+                ContextCompat.getColor(requireContext(), R.color.no_color)
+            no.outlineSpotShadowColor =
+                ContextCompat.getColor(requireContext(), R.color.no_color)
+            yes.outlineAmbientShadowColor = ContextCompat.getColor(requireContext(), R.color.red)
+            yes.outlineSpotShadowColor = ContextCompat.getColor(requireContext(), R.color.red)
+            maybe.outlineAmbientShadowColor =
+                ContextCompat.getColor(requireContext(), R.color.maybe_color)
+            maybe.outlineSpotShadowColor =
+                ContextCompat.getColor(requireContext(), R.color.maybe_color)
         }
     }
 }
