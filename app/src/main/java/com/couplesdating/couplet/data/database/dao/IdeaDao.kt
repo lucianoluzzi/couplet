48adc8ec07bd9aef25dde30b2bcc7f57e832db8f
Luciano Luzzi <luciano@woovapp.com>
2021-07-22 23:03:21 +0200

    Fixing insert crash

--- app/src/main/java/com/couplesdating/couplet/data/database/dao/IdeaDao.kt
+++ app/src/main/java/com/couplesdating/couplet/data/database/dao/IdeaDao.kt
@@ -3,12 +3,13 @@ package com.couplesdating.couplet.data.database.dao
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import com.couplesdating.couplet.data.database.entities.IdeaEntity
 
 @Dao
 interface IdeaDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(ideas: List<IdeaEntity>)
 
     @Delete
